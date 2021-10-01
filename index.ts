cd32ca89f0e9031ed8b0abe3475658686f304aab
papob.boonpat <boonpat.papob@gmail.com>
2021-10-01 15:56:35 +0700

    Change to typescript

--- index.ts
+++ index.ts
@@ -1,18 +1,20 @@
+import express, { Request, Response } from "express";
+
 const mysql = require("mysql");
-const express = require("express");
+
 const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "./build")));
+app.use(express.static(path.resolve(__dirname, "./simple-react/build")));
 
-app.get("/aaa", (req, res) => {
+app.get("/aaa", (req: Request, res: Response) => {
   res.status(200);
   res.json({ message: "test" });
 });
 
-app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./build/index.html"));
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.resolve(__dirname, "./simple-react/build/index.html"));
 });
 
 app.listen(process.env.PORT || 3001, () => {
